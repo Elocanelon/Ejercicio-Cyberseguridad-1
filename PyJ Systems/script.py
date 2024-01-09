f345dd26c4518425d08419bd99fede9a192a2be8
Lucas Macias <chueko@gmail.com>
2024-01-09 17:21:20 -0300

    Finish Exercise 1

--- PyJ Systems/script.py
+++ PyJ Systems/script.py
@@ -1,5 +1,3 @@
-# Script para generar hash md5
-
 import hashlib
 
 def calcular_md5sum(ruta_archivo, tamano_bloque=65536):
@@ -15,6 +13,14 @@ def calcular_md5sum(ruta_archivo, tamano_bloque=65536):
     except Exception as e:
         print(f"Error al calcular el hash MD5: {e}")
         return None
+
+#La funcion calcular_md5sum solicita como parámetro, ruta_archivo, y tamano_bloque el cual ya tiene asignado 65536 por defecto
+# En el if del final, en caso de existir hash_md5, nos imprime el archivo y su hash. Para eso hay que darle un valor a archivo_a_hash
+# con el path y el archivo a hashear, y por otro lado a hash_md5 le damos el valor que nos devuelva la funcion al hashear el archivo.
+# Luego de chequear uno por uno, he visto que unicamente el log.txt difiere con los has que se encuentran en la consigna
+# dando por hecho que ha sido el archivo alterado en cuestión.
+archivo_a_hash = "log.txt" 
+hash_md5 = calcular_md5sum(archivo_a_hash)
 
 if hash_md5:
     print(f"El hash MD5 del archivo '{archivo_a_hash}' es: {hash_md5}")
