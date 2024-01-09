f200adc8c18c56adbe2acdc396d0eed1c20f05f8
Gabriel Perez Federico <perezgaa@globalhitss.com>
2024-01-09 20:58:50 -0300

    Add: Generate and compare new hashes vs old hashes using a python script

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import hashlib
+import json
+
+
+def generate_files_md5(path):
+    new_hashes = {}
+    for file in os.listdir(path):
+        with open(f'{path}/{file}', 'rb') as f:
+            bytes = f.read()
+            md5_hash = hashlib.md5(bytes).hexdigest()
+            new_hashes[file] = md5_hash
+    return new_hashes
+
+
+def compare_files_hashes(new_hashes, old_hashes):
+    for file, md5_hash in new_hashes.items():
+        if md5_hash == old_hashes[file]:
+            print(f'{file} OK')
+        else:
+            print(f'{file} FAIL')
+
+
+if __name__ == '__main__':
+    path = 'PyJ Systems'
+    new_hashes = generate_files_md5(path)
+    old_hashes = json.load(open('old_hashes.json', 'r'))
+    compare_files_hashes(new_hashes, old_hashes)
